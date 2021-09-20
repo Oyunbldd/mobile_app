47501c34e7d638001b3991ddb15379e77a9d4a85
Oyunbldd <ganboldoyunbold914@gmail.com>
2021-09-20 17:40:26 +0800

    mission-2

--- lib/main.dart
+++ lib/main.dart
@@ -13,102 +13,33 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        backgroundColor: Colors.grey,
       ),
       home: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.yellow,
-              child: Container(
-                margin: EdgeInsets.fromLTRB(10, 50, 0, 0),
-                child: Text(
-                  'H:100',
+        backgroundColor: Colors.grey[800],
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Text(
+                  'Movies',
                   style: TextStyle(
-                    fontSize: 15,
-                  ),
+                      fontSize: 28,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
                 ),
-              ),
-            ),
-            Flexible(
-              flex: 1,
-              child: Container(
-                child: Row(
+                ListView(
                   children: [
-                    Flexible(
-                      flex: 1,
-                      child: Container(
-                        color: Colors.blue,
-                        child: Center(
-                          child: Text('Row - Flex:1'),
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      flex: 3,
-                      child: Container(
-                        child: Column(
-                          children: [
-                            Container(
-                              width: double.infinity,
-                              height: 120,
-                              color: Colors.grey,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container(
-                                    child: Text('Row - Flex :3'),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.only(top: 35),
-                                    child: Text('H:120'),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Flexible(
-                              flex: 1,
-                              child: Container(
-                                width: double.infinity,
-                                height: double.infinity,
-                                color: Colors.green,
-                                child: Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: Text(
-                                    'Col -Flex :1',
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: double.infinity,
-                              height: 150,
-                              child: Align(
-                                child: Text('H:150'),
-                                alignment: Alignment.centerLeft,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                    Container(
+                      height: 150,
+                      // decoration: BoxDecoration(bo),
                     ),
                   ],
-                ),
-              ),
+                )
+              ],
             ),
-            Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.red,
-              child: Center(
-                child: Text(
-                  'H:100',
-                  style: TextStyle(fontSize: 15),
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
