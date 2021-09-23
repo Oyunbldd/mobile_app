7fb4b51ccd67fa9818f0e8bb701c39021c735c18
Oyunbldd <oyunbldd@bayaraaEs-iMac.local>
2021-09-23 19:01:53 +0800

    flip

--- App.js
+++ App.js
@@ -6,34 +6,53 @@ import {
   StyleSheet,
   Button,
   SafeAreaView,
+  Easing,
 } from 'react-native';
 
 const App = () => {
-  const translation = useRef(new Animated.ValueXY({x: 0, y: 0})).current;
+  const translation = useRef(new Animated.Value(0)).current;
   useEffect(() => {
     Animated.sequence([
       Animated.timing(translation, {
-        toValue: {x: 290, y: 350},
-        duration: 2000,
+        toValue: 0,
+        duration: 1000,
+        easing: Easing.linear,
         useNativeDriver: true,
       }),
       Animated.timing(translation, {
-        toValue: {x: 0, y: 650},
-        duration: 2000,
+        toValue: 1,
+        duration: 900,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+      Animated.timing(translation, {
+        toValue: 2,
+        duration: 900,
+        useNativeDriver: true,
+      }),
+      Animated.timing(translation, {
+        toValue: 3,
+        duration: 900,
+        useNativeDriver: true,
+      }),
+      Animated.timing(translation, {
+        toValue: 4,
+        duration: 900,
         useNativeDriver: true,
       }),
     ]).start();
   }, [translation]);
+  const spin = translation.interpolate({
+    inputRange: [0, 1, 2, 3, 4],
+    outputRange: ['0deg', '90deg', '180deg', '270deg', '360deg'],
+  });
   return (
     <SafeAreaView>
       <Animated.View
         style={[
           styles.animation,
           {
-            transform: [
-              {translateX: translation.x},
-              {translateY: translation.y},
-            ],
+            transform: [{rotateY: spin}],
           },
         ]}
       />
@@ -41,6 +60,12 @@ const App = () => {
   );
 };
 const styles = StyleSheet.create({
-  animation: {backgroundColor: 'deeppink', height: 100, width: 100},
+  animation: {
+    backgroundColor: 'deeppink',
+    height: 100,
+    width: 100,
+    borderRadius: 10,
+    margin: 100,
+  },
 });
 export default App;
